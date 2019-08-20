78a0f42390670aeb8dbcf2ea08a1667a4b5cab90
Mikael Davidsson <mikael.davidsson@digitalroute.com>
2019-08-20 12:57:09 +0200

    Cleanup.

--- pkg/server/cognitoAuthorizer.go
+++ pkg/server/cognitoAuthorizer.go
@@ -19,39 +19,26 @@ type CognitoAuthorizer struct {
 }
 
 func (c *CognitoAuthorizer) authorize(req *http.Request) bool {
-	fmt.Printf("Request headers: %+v\n", req.Header)
 	auth := req.Header.Get("Authorization")
 	if len(auth) <= len("Bearer ") {
-		fmt.Println("Failed to get Bearer token " + auth)
 		return false
 	}
 	authToken := auth[len("Bearer "):len(auth)]
-	E := "AQAB"
-	//E := "AQAC"
-	N := "u-_nfb3nv38zEs9FbkK8CGg5_hhK0DubPB_GEArv3hezDk0FwAzQ6dO1lkITtPOtzwaY-pitG16VttVvjKFYzPM7bq-r0ibNumAbEV90aa0t41DlhHTyt-5j3toQeaCYYJgSDYgsJOgMEyRM1UM3cgCWVQGk0sjgNFnvUaGdeKeUQpupXmyLJ6E5Q5Gjl7NJjGne67xyNElZpxh7AlIV0aKZJnC-Devz7KOLE5QPWV6QRFABtXcBGeUvAAVkq8Txjdj_rJLN2ftjOFpBgG67QcnV0CujqslphvjjyDuQ3E1EEsQIH5N49hzd5KsALUIg7t4keN3WnU0dt2tyUlFjYQ"
-	// Parse takes the token string and a function for looking up the key. The latter is especially
-	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
-	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
-	// to the callback, providing flexibility.
+
 	token, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
-		key := c.convertKey(E, N)
+		key := c.convertKey(c.jwks.Keys[1].E, c.jwks.Keys[1].N)
 		return key, nil
 	})
 
-	if token.Valid {
-		//fmt.Println(token.Raw)
-		fmt.Printf("Claims: %+v\n", token.Claims)
-		fmt.Printf("Header: %+v\n", token.Header)
-		//fmt.Println(token.Claims)
-		return true
-	} else {
-		fmt.Printf("Claims: %+v\n", err)
+	if err != nil || !token.Valid {
 		return false
+	} else {
+		//fmt.Printf("Claims: %+v\n", token.Claims)
+		return true
 	}
 }
 
 func (c *CognitoAuthorizer) convertKey(rawE, rawN string) *rsa.PublicKey {
-	fmt.Println("+++++++++++++++++++++++++convertKey")
 	decodedE, err := base64.RawURLEncoding.DecodeString(rawE)
 	if err != nil {
 		panic(err)
